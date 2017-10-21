38a248bd36d03b6c62efd73bc5c028a84a2a80ff
Alexandre Martins <alexandremartins@gmail.com>
2017-10-21 16:08:17 -0300

    inclusion of postgres config

---
+++ src/main/java/com/letsplay/server/DataSourceConfig.java
@@ -0,0 +1,51 @@
+package com.letsplay.server;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class DataSourceConfig {
+
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    @Profile("postgres")
+    public DataSource postgresDataSource() {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        log.info("Initializing PostgreSQL database: {}", databaseUrl);
+
+        URI dbUri;
+        try {
+            dbUri = new URI(databaseUrl);
+        }
+        catch (URISyntaxException e) {
+            log.error(String.format("Invalid DATABASE_URL: %s", databaseUrl), e);
+            return null;
+        }
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' 
+                       + dbUri.getPort() + dbUri.getPath();
+
+        org.apache.tomcat.jdbc.pool.DataSource dataSource 
+            = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
+        return dataSource;
+    }
+}
